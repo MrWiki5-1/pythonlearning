f53e52b3bd9e8b50c329d4cbaff64d98277d8264
MrWiki5-1 <estebanvelz2006@gmail.com>
2023-01-27 15:23:01 +0000

    update

---
+++ ejercios.py
@@ -0,0 +1,25 @@
+contador=0
+miemalil=input("Pe CTM coloca tu direcion de email XDDDD: ")
+
+for i in miemalil:
+
+    if(i=="@" or i=="."):
+
+        contador=contador=1
+
+if contador==2:
+    print("Wow tu si le sabes no eres un robot :D")
+else:
+    ("ajajaajja")
+
+print("JAJJAJAJAJ apoo esa direccion email existe CTM ")
+
+
+
+
+
+
+
+
+  
+
